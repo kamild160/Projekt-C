4f83ca36376950b596109c8216eff799e7737637
kamild160 <293206@uwr.edu.pl>
2017-11-08 19:41:52 +0100

    Początek tworzenia gry
    dodane:
    -próbny tor do przejścia
    - podstawowe sterowanie za pomocą strzałek
    -ruch kamery względem piłeczki
    - obrót piłeczki
    - podstawowa fizyka

---
+++ Assets/camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+    public Transform sphere;
+
+	
+	
+	void Update ()
+    {
+        Rigidbody rigidbody = sphere.GetComponent<Rigidbody>(); //pobiera komponent fizyki
+        
+        Vector3 vector = new Vector3(0, 5f, -7f); //nowa pozycja kamery
+        float velocity = rigidbody.velocity.sqrMagnitude; //pobiera prędkość kuli 
+        vector = vector * (1f + velocity / 25f); //prękośc kamery zależy od prędkości kuli
+        Vector3 newPosition = sphere.position + vector; //oblicza nową pozycje kamery
+
+        
+        
+        transform.position = Vector3.Lerp(transform.position, newPosition,Time.deltaTime*4f); //nadaje płynność poruszania się kamery
+        transform.LookAt(sphere); //kamera "patrzy" na środek kuli
+	}
+}
---
+++ Assets/spherecontrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spherecontrol : MonoBehaviour
+{
+
+
+	
+	
+	void Update ()
+    {
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();//pobiera komponent fizyki
+
+        Vector3 direction=Vector3.zero; //zmienna kierunku
+
+        //sterowanie 
+
+		if(Input.GetKey(KeyCode.UpArrow))
+        {
+            direction =-Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction= Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+           direction= Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+           direction= -Vector3.forward;
+        }
+        rigidbody.AddTorque(direction * 10f); //do komponentu fizyki odaje moment obrotowy
+    }
+}
