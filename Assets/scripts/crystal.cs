42a58c39b613258c43b96ff09870df3717c7a4ae
kamild160 <293206@uwr.edu.pl>
2018-01-02 14:51:10 +0100

    Kryształy
    dodano:
    -możliwość śmierci po upadku 
    -kryształy do zbierania: po zebraniu wszystkich kończy się poziom
    -nazwę poziomu
    -animacje

---
+++ Assets/scripts/crystal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class crystal : MonoBehaviour
+
+{
+    public GameObject particles; 
+
+
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name != "sphere")  // sprawdza czy obiakt zderzył się z kulą
+        {
+
+            return;
+
+        }
+
+        if( crystalsleft()==1)
+        {
+            SceneManager.LoadScene("win"); // po zebranu sszystkich elementów
+        }
+        else 
+        {
+            Instantiate(particles,transform.position,Quaternion.identity); //działanie zrobłysku
+            Destroy(gameObject, 0.1f);
+        }
+     }
+
+
+
+    int crystalsleft()
+    {
+        crystal[] crystals= Component.FindObjectsOfType<crystal>();
+        return crystals.Length;
+    }
+}
---
+++ Assets/scripts/levelname.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelname : MonoBehaviour
+{
+
+    public TextMesh textMesh;
+
+    void Start()
+    {
+        string LevelName = Application.loadedLevelName; //pobiera nazwę sceny i wyświetla ją
+        textMesh.text = LevelName;
+
+    }
+
+}	
+		
+
